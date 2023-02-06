58d3ec611809bb394bac1d65f6a3d29df0a8a6e1
Richard Kang <kangrc1@gmail.com>
2023-02-06 10:49:18 -0500

    Added tests

--- Server.Tests/StatusTests.cs
+++ Server.Tests/StatusTests.cs
@@ -1,11 +1,31 @@
-﻿namespace Server.Tests
+﻿using Server.DataAccessObjects;
+using Server.Enums;
+
+namespace Server.Tests
 {
     public class StatusTests
     {
         [Fact]
         public void GetAllStatuses()
         {
+            StatusDao dao = new();
 
+            IEnumerable<StatusType> statuses = dao.GetAll();
+
+            Assert.NotNull(statuses);
+        }
+
+        [Fact]
+        public void CheckEnumExistsInDatabase()
+        {
+            StatusDao dao = new();
+
+            IEnumerable<StatusType> statuses = dao.GetAll();
+
+            foreach (StatusType statusType in Enum.GetValues(typeof(StatusType)))
+            {
+                Assert.Contains(statusType, statuses);
+            }
         }
     }
 }
